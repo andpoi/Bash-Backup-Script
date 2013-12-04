4530fdcc36bdc7c9629f609b60078f3cfec817d7
Andrew Poirier <andpoi@gmail.com>
2013-12-04 15:57:59 -0500

    Explains to the user whats going on

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 USERD=/Volumes/data/Employees/$USER
 BACKUPLOC=/Users/$USER/Dropbox/my-backup-$(date +%Y%m%d).tgz
 
+echo "Currently tarballing your files and moving them to" $BACKUPLOC
 tar -czf $BACKUPLOC $USERD
 
 echo "Backup created at" $BACKUPLOC
