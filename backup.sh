06cc5d2a7679dde099f9c035fdc7412db3ccdea4
Andrew Poirier <andpoi@gmail.com>
2013-12-04 15:15:04 -0500

    Fixed line to use less text

--- backup.sh
+++ backup.sh
@@ -3,7 +3,6 @@
 USERD=/Volumes/data/Employees/$USER
 BACKUPLOC=/Users/$USER/Dropbox/my-backup-$(date +%Y%m%d).tgz
 
-tar -czf $BACKUPLOC /Volumes/data/Employees/$USER
 tar -czf $BACKUPLOC $USERD
 
 echo "Backup created at" $BACKUPLOC
