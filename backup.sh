03de3d9851e81af2315d09f2a4c98d96ae77445a
Andrew Poirier <andpoi@gmail.com>
2013-12-05 12:23:21 -0500

    Checks if file exists and quits if it does

--- backup.sh
+++ backup.sh
@@ -2,9 +2,23 @@
 
 USERD=/Volumes/data/Employees/$USER
 BACKUPLOC=/Users/$USER/Dropbox/my-backup-$(date +%Y%m%d).tgz
+SEP="========================================================================"
+
+if [ -f $BACKUPLOC ]
+	then
+		echo $SEP
+		echo "The file" $BACKUPLOC "already exists...."
+		echo "Exiting this program so you don't have to"
+		echo $SEP
+		exit
+	else
+		echo $SEP
+		echo "You haven't backed up yet today.... Let's do that right now"
+fi
 
 echo "Currently tarballing your files and moving them to" $BACKUPLOC
 tar -czf $BACKUPLOC $USERD
 
 echo "Backup created at" $BACKUPLOC
 echo "Created from files located at" $USERD "for user" $USER
+echo $SEP
